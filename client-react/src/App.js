02be8bd97ab727e69cfd2b71431eede04b43bdc0
DannyDoneva96 <danielasinmas1@gmail.com>
2023-01-23 20:41:55 +0200

    Home layout finished

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,9 +1,9 @@
 import './App.scss';
-
+import Home from './components/Home/Home'
 function App() {
   return (
     <div className="App">
-      
+      <Home/>
     </div>
   );
 }
--- client-react/src/components/Home/Home.js
+++ client-react/src/components/Home/Home.js
@@ -3,8 +3,15 @@ import './Home.scss'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className="home-container">
+      <div className="main-content">
+        <section className="menu">
+          <div className="container-to-fl">
+          <button className="options">PLAY</button>
+          <button className="options">Settings</button>
+          <button className="options">Rules</button></div>
+        </section>
+      </div>
     </div>
   )
 }
