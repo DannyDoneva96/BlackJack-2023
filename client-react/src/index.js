415066283029cde28bbc7b71164aefa22d7bb457
Martin B <85784810+SpooRe91@users.noreply.github.com>
2023-01-24 13:32:42 +0200

    Update index.js
    switched the places of Browser router and provider

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -9,11 +9,11 @@ import { store } from "./App/store";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>  
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
