48e2bc6922669a7036b8d054d7f21b31a74f22a6
Martin B <m.bogdanov9110@gmail.com>
2023-01-24 09:24:03 +0200

    added note for the laoder in App.js

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -3,17 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home'
 import Game from './components/Game.js/Game'
 import Settings from './components/Settings/Settings'
-import Loading from './components/Loading/Loading'
+// import Loading from './components/Loading/Loading'
 function App() {
   return (
     <div className="App">
-     <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/game" element={<Game />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/loading" element={<Loading />} />
-
-       </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/settings" element={<Settings />} />
+        {/* този лоудър е готин, но трябва да го направим да е глобален и да се слага когато има навигиране или зарежда*/}
+        {/* <Route path="/loading" element={<Loading />} />  */}
+      </Routes>
     </div>
   );
 }
