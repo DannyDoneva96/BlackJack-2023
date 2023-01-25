8e87ea36a5716c7d3902ee78da536b34d6b9e971
Martin B <85784810+SpooRe91@users.noreply.github.com>
2023-01-25 23:14:29 +0000

    refactored the scss to look like and actual scss with nested attributes, fixed some English words

--- client-react/src/components/ChashPopUp/CashPopUp.js
+++ client-react/src/components/ChashPopUp/CashPopUp.js
@@ -12,25 +12,23 @@ import './CashPopUp.scss'
 import imgbj from '../../assets/images/1470895f3b5a7c0e680efc78647dbe1f___Копие-removebg-preview.png'
 
 
-
-
 const CashPopUp = ({ setisVisible }) => {
 
-  const chipAdd = useSelector((state) => state.playerChipAdd);
+  // const chipAdd = useSelector((state) => state.playerChipAdd);
+  //tuk nqma da ni trqbva useSelector-a osven, ako nqma da pokazvame kakva stoinost ima predi da zapochne igrat
 
   const dispatch = useDispatch();
-
 
   return (
     <div className="cash-pop-up">
       <div className="container-cp">
         <img src={imgbj} alt="" />
-        <h3 className="cp-h">Initial Cash:</h3>
+        <h3 className="cp-h">Choose your initial cash:</h3>
         <div className="btn-cont-popup">
-          <button className="start-chips-btn" onClick={() => dispatch(chipFiveAdd(), setisVisible(false))}>Add 5 by each chips</button>
-          <button className="start-chips-btn" onClick={() => dispatch(chipTenAdd(), setisVisible(false))}>Add 10 by each chips</button>
-          <button className="start-chips-btn" onClick={() => dispatch(chipTwentyAdd(), setisVisible(false))}>Add 20 by each chips</button>
-          <button className="start-chips-btn" onClick={() => dispatch(chipFiftyAdd(), setisVisible(false))}>Add 50 by each chips</button>
+          <button className="start-chips-btn" onClick={() => dispatch(chipFiveAdd(), setisVisible(false))}>Add 5 of each chips</button>
+          <button className="start-chips-btn" onClick={() => dispatch(chipTenAdd(), setisVisible(false))}>Add 10 of each chips</button>
+          <button className="start-chips-btn" onClick={() => dispatch(chipTwentyAdd(), setisVisible(false))}>Add 20 of each chips</button>
+          <button className="start-chips-btn" onClick={() => dispatch(chipFiftyAdd(), setisVisible(false))}>Add 50 of each chips</button>
         </div>
       </div>
 
