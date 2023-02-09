643d2b8c127a23368f07adac7d0b323f56e47459
Martin B <m.bogdanov9110@gmail.com>
2023-02-09 18:46:45 +0200

    updated imports

--- client-react/src/components/Game.js/BetChipView.js
+++ client-react/src/components/Game.js/BetChipView.js
@@ -1,26 +1,26 @@
 import { nanoid } from '@reduxjs/toolkit';
-import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import {
-    LastOneRemove, playerChips,
+    LastOneRemove, gameState,
 } from "../../features/gamePlay/playerSlice";
 
 
 const BetChipView = () => {
 
     const dispatch = useDispatch();
-    const chipAdd = useSelector(playerChips);
+    const chipAdd = useSelector(gameState);
 
     let counter = 1
 
     return (
-        <div className="chip-bet-fl-c"  onClick={() => dispatch(LastOneRemove())}>
-            
-            {chipAdd.bet.map((x) =><img key={nanoid(5)} className="smallChipsBets" src={x} 
-             style={{position:'absolute',left:`${counter+=20}px`}} alt="" />
+        <div className="chip-bet-fl-c" onClick={() => dispatch(LastOneRemove())}>
+
+            {chipAdd.bet.map((x) => <img key={nanoid(5)} className="smallChipsBets" src={x}
+                style={{ position: 'absolute', left: `${counter += 20}px` }} alt="" />
 
             )}
-              
+
         </div>
     )
 }
