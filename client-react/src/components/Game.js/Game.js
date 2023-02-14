127d97018897352858209fb2e1f41ddd6d51d260
DannyDoneva96 <danielasinmas1@gmail.com>
2023-02-14 11:38:02 +0200

    change  the conditional statements according to the game rules

--- client-react/src/components/Game.js/Game.js
+++ client-react/src/components/Game.js/Game.js
@@ -22,7 +22,9 @@ import {
   getOneDealerCard,
   getDealerCards,
   getPlayerCards,
-  nextGame
+  nextGame,
+  isBJPlayer,
+  isBJDealer
 } from "../../features/gamePlay/playerSlice";
 
 
@@ -152,29 +154,32 @@ const Game = () => {
             {
               whilePlaying //if playing the game
                 ?
-                chipAdd.playerCardsValue === 21 // if the player has 21 points
+                chipAdd.isBJPlayer === true  && chipAdd.isBJDealer === false// if the player has BJ 
                   ?
                   <>
-                    <p>You WIN! You have reached 21 points! </p> {/*the player wins*/}
+                    <p>BlackJasck! </p> {/*the player wins*/}
+                    <p>You win {chipAdd.total + chipAdd.total / 2}$ !</p>
+                    <button onClick={() =>
+                              [playAgain(), setToggleValue(state => true)]}>PLAY AGAIN!</button>
+                       
                   </>
                   :
-                  chipAdd.dealerCardsValue === 21 // if the dealer has 21 points, the player loses
+                  chipAdd.isBJDealer === true &&  chipAdd.isBJPlayer === false// if the dealer has BJ
                     ?
                     <>
-                      <p>Dealer has 21 points, you lose!</p> {/*the player loses*/}
+                      <p>Dealer Wins!</p> {/*the player loses*/}
                     </>
                     :
                     chipAdd.dealerCardsValue > 21
                       ?
                       <>
-                        <p>Dealer has more than 21 points, YOU WIN!</p>
+                        <p>You win {chipAdd.total}$ !</p>
                       </>
                       :
                       chipAdd.playerCardsValue > 21//if the player has more than 21 points
                         ?
                         <div>
-                          <p>You lose! You have {chipAdd.playerCardsValue} points!</p>
-                          <p>Better luck next time!</p>
+                          <p>Dealer Wins!</p>
                         </div>
                         :
                         (!isOver && isStanding)
